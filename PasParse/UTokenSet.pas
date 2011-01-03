53515535d8a43c01e7deb92df04a999843283ff3
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-03 12:44:29 +0100

    UTokenSet: Added documentation

--- PasParse/UTokenSet.pas
+++ PasParse/UTokenSet.pas
@@ -6,16 +6,21 @@ uses
   UTokenType;
 
 type
+  /// A named set of token types
   TTokenSet = class
   private
     FTokenSet: set of TTokenType;
     FName: string;
 
   public
+    /// Standard constructor. Creates an empty set.
     constructor Create(AName: string);
 
+    /// Add one token type to the set
     procedure Add(ATokenType: TTokenType);
+    /// Add a range of token types to the set
     procedure AddRange(ATokenSet: TTokenSet);
+    /// Checks whether the set contains the given token type
     function Contains(ATokenType: TTokenType): Boolean;
   end;
 
@@ -25,6 +30,7 @@ implementation
 
 procedure TTokenSet.Add(ATokenType: TTokenType);
 begin
+  // Add given token type to the set
   Include(FTokenSet, ATokenType);
 end;
 
@@ -32,9 +38,12 @@ procedure TTokenSet.AddRange(ATokenSet: TTokenSet);
 var
   ATokenType: TTokenType;
 begin
+  // Iterate through all available token types
   for ATokenType := Low(TTokenType) to High(TTokenType) do
   begin
+    // If the given token set contains the current token type
     if ATokenSet.Contains(ATokenType) then
+      // Add the token type to our own token set too
       Add(ATokenType);
   end;
 end;
