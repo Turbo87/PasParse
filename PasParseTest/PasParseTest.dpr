cf839ea73844cb81bcd9020c91d74675e2b79443
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-03 18:14:48 +0100

    UTestDictionary: Added TDictionary unit test

--- PasParseTest/PasParseTest.dpr
+++ PasParseTest/PasParseTest.dpr
@@ -5,10 +5,12 @@ program PasParseTest;
 uses
   SysUtils,
   UTestLexScanner in 'UTestLexScanner.pas',
-  UTest in 'UTest.pas';
+  UTest in 'UTest.pas',
+  UTestDictionary in 'UTestDictionary.pas';
 
 begin
   try
+    TTestDictionary.Test;
     TTestLexScanner.Test;
   except
     on E:Exception do
---
+++ PasParseTest/UTestDictionary.pas
@@ -0,0 +1,65 @@
+unit UTestDictionary;
+
+interface
+
+uses
+  UTest;
+
+type
+  TTestDictionary = class(TTest)
+  public
+    class procedure TestAll; override;
+    class function GetName: string; override;
+  end;
+
+implementation
+
+uses
+  UDictionary, SysUtils;
+
+{ TTestDictionary }
+
+class function TTestDictionary.GetName: string;
+begin
+  Result := 'Dictionary';
+end;
+
+class procedure TTestDictionary.TestAll;
+var
+  ADict: TDictionary;
+  AObject: TObject;
+begin
+  ADict := TDictionary.Create;
+
+  // No key-value pairs yet
+  OK(not ADict.Contains('test1'), 'Contains');
+  OK(not ADict.Read('test1', AObject), 'Read');
+  OK(ADict.Read('test2') = nil, 'Read');
+
+  // Write first pair
+  OK(not ADict.Write('test1', TObject(5)), 'Write');
+
+  // Read first pair
+  OK(ADict.Contains('test1'), 'Contains');
+  OK(ADict.Read('test1', AObject), 'Read');
+  OK(Integer(AObject) = 5, 'Integer(AObject) = 5');
+
+  // Write second pair
+  OK(not ADict.Write('test2', TObject(1)), 'Write');
+
+  // Read second pair
+  OK(ADict.Read('test2') = TObject(1), 'Read');
+
+  // Overwrite first pair
+  OK(ADict.Write('test1', TObject(3)), 'Write');
+
+  // Read first pair again
+  OK(ADict.Contains('test1'), 'Contains');
+  OK(ADict.Read('test1', AObject), 'Read');
+  OK(Integer(AObject) = 3, 'Integer(AObject) = 3');
+
+  FreeAndNil(ADict);
+end;
+
+end.
+
