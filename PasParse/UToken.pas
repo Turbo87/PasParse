bab95a98f53d6eb79ae76094e590ba234c2e05ca
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-03 19:21:29 +0100

    UToken: Derive TToken class from TASTNode

--- PasParse/UToken.pas
+++ PasParse/UToken.pas
@@ -3,28 +3,25 @@ unit UToken;
 interface
 
 uses
-  UTokenType, ULocation;
+  UTokenType, ULocation, UASTNode;
 
 type
   /// Represents a token including it's text, positions and token type
-  TToken = class
+  TToken = class(TASTNode)
   private
     FLocation: TLocation;
     FParsedText: string;
     FText: string;
     FTokenType: TTokenType;
 
-    function GetEndLocation: TLocation;
+    function GetLocation: TLocation; override;
+    function GetEndLocation: TLocation; override;
 
   public
     /// Standard constructor
     constructor Create(ATokenType: TTokenType; ALocation: TLocation;
                        AText, AParsedText: string);
 
-    /// Location at the start of the token
-    property Location: TLocation read FLocation;
-    /// Location at the end of the token
-    property EndLocation: TLocation read GetEndLocation;
     /// Parsed text if available, otherwise empty string ''
     property ParsedText: string read FParsedText;
     /// Textual representation of the token
@@ -51,6 +48,11 @@ begin
   // Create a new TLocation instance at the end position of the token
   Result := TLocation.Create(FLocation.FileName, FLocation.FileSource,
                              FLocation.Offset + Length(FText));
+end;
+
+function TToken.GetLocation: TLocation;
+begin
+  Result := FLocation;
 end;
 
 end.
