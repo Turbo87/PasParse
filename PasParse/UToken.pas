efba70f6a7506e9b02c7ddbaaab50594129246e2
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-03 12:40:10 +0100

    UToken: Added documentation

--- PasParse/UToken.pas
+++ PasParse/UToken.pas
@@ -6,6 +6,7 @@ uses
   UTokenType, ULocation;
 
 type
+  /// Represents a token including it's text, positions and token type
   TToken = class
   private
     FLocation: TLocation;
@@ -16,13 +17,19 @@ type
     function GetEndLocation: TLocation;
 
   public
+    /// Standard constructor
     constructor Create(ATokenType: TTokenType; ALocation: TLocation;
                        AText, AParsedText: string);
 
+    /// Location at the start of the token
     property Location: TLocation read FLocation;
+    /// Location at the end of the token
     property EndLocation: TLocation read GetEndLocation;
+    /// Parsed text if available, otherwise empty string ''
     property ParsedText: string read FParsedText;
+    /// Textual representation of the token
     property Text: string read FText;
+    /// Type of the token
     property TokenType: TTokenType read FTokenType;
   end;
 
@@ -41,6 +48,7 @@ end;
 
 function TToken.GetEndLocation: TLocation;
 begin
+  // Create a new TLocation instance at the end position of the token
   Result := TLocation.Create(FLocation.FileName, FLocation.FileSource,
                              FLocation.Offset + Length(FText));
 end;
