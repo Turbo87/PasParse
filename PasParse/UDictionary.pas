2fe027b77c7e3efc6f7e1c9f09c0af918c2ed466
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-03 18:14:25 +0100

    UDictionary: Added basic dictionary implementation based on TStringList

---
+++ PasParse/UDictionary.pas
@@ -0,0 +1,99 @@
+unit UDictionary;
+
+interface
+
+uses
+  Classes;
+
+type
+  TDictionary = class
+  private
+    /// Internal list of the dictionary
+    FList: TStringList;
+
+    /// Returns the integer-based index of the given key or -1 if not found
+    function IndexOf(AKey: string): Integer;
+
+  public
+    /// Standard constructor
+    constructor Create;
+    /// Standard destructor
+    destructor Destroy;
+
+    /// Returns true if the given key was found in the dictionary and writes
+    ///  the corresponding TObject instance to the AValue output parameter
+    function Read(AKey: string; out AValue: TObject): Boolean; overload;
+    /// Returns the TObject instance corresponding with the given key or
+    ///  nil if not found
+    function Read(AKey: string): TObject; overload;
+    /// Assigns the given TObject to the given key.
+    ///  Returns whether the key already existed before.
+    function Write(AKey: string; AValue: TObject): Boolean;
+
+    /// Tests whether a key exists in the dictionary
+    function Contains(AKey: string): Boolean;
+  end;
+
+implementation
+
+uses
+  SysUtils;
+
+{ TDictionary }
+
+function TDictionary.Contains(AKey: string): Boolean;
+var
+  AIndex: Integer;
+begin
+  AIndex := IndexOf(AKey);
+  Result := (AIndex >= 0);
+end;
+
+constructor TDictionary.Create;
+begin
+  FList := TStringList.Create;
+end;
+
+destructor TDictionary.Destroy;
+begin
+  FreeAndNil(FList);
+end;
+
+function TDictionary.IndexOf(AKey: string): Integer;
+begin
+  Result := FList.IndexOf(AKey);
+end;
+
+function TDictionary.Read(AKey: string; out AValue: TObject): Boolean;
+var
+  AIndex: Integer;
+begin
+  AIndex := IndexOf(AKey);
+  Result := (AIndex >= 0);
+  if Result then
+    AValue := FList.Objects[AIndex]
+end;
+
+function TDictionary.Read(AKey: string): TObject;
+var
+  AValue: TObject;
+begin
+  if Read(AKey, AValue) = True then
+    Result := AValue
+  else
+    Result := nil;
+end;
+
+function TDictionary.Write(AKey: string; AValue: TObject): Boolean;
+var
+  AIndex: Integer;
+begin
+  AIndex := IndexOf(AKey);
+  Result := (AIndex >= 0);
+  if Result then
+    FList.Objects[AIndex] := AValue
+  else
+    FList.AddObject(AKey, AValue);
+end;
+
+end.
