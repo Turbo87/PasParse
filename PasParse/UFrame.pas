800ec4de64e339d169e897c095fab839b981f7be
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-05 13:33:18 +0100

    UFrame: Added abstract TFrame class

---
+++ PasParse/UFrame.pas
@@ -0,0 +1,30 @@
+unit UFrame;
+
+interface
+
+uses
+  ULocation, UTokenType, UTokenSet, UToken;
+
+type
+  TFrame = class
+  private
+    function GetDisplayName: string; virtual; abstract;
+    function GetIsEOF: Boolean; virtual; abstract;
+    function GetLocation: TLocation; virtual; abstract;
+    function GetNext: TFrame; virtual; abstract;
+    function GetTokenType: TTokenType; virtual; abstract;
+
+  public
+    property DisplayName: string read GetDisplayName;
+    property IsEOF: Boolean read GetIsEOF;
+    property Location: TLocation read GetLocation;
+    property Next: TFrame read GetNext;
+    property TokenType: TTokenType read GetTokenType;
+
+    function CanParseToken(ATokenSet: TTokenSet): Boolean; virtual; abstract;
+    function ParseToken(ATokenSet: TTokenSet): TToken; virtual; abstract;
+  end;
+
+implementation
+
+end.
