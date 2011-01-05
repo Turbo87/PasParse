ea4356d40969c096f596d90c893da5d21e9702d9
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-05 13:47:44 +0100

    UInvalidOperationException: Added EInvalidOperationException class

---
+++ PasParse/UInvalidOperationException.pas
@@ -0,0 +1,15 @@
+unit UInvalidOperationException;
+
+interface
+
+uses
+  SysUtils;
+
+type
+  /// A special type of exception
+  EInvalidOperationException = class(Exception)
+  end;
+
+implementation
+
+end.
