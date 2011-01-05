feafce665986b3a1706b7cc815f3644fd54de98c
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-05 12:34:26 +0100

    UTestASTNode: Added InspectNestedBinaryOperationNodes unit test

--- PasParseTest/UTestASTNode.pas
+++ PasParseTest/UTestASTNode.pas
@@ -15,6 +15,8 @@ type
     class function TestInspectToken: Boolean;
     /// Tests if TBinaryOperationNode.Inspect() works as expected
     class function TestInspectBinaryOperationNode: Boolean;
+    /// Tests if TBinaryOperationNode.Inspect() works as expected
+    class function TestInspectNestedBinaryOperationNodes: Boolean;
 
   protected
     class procedure TestAll; override;
@@ -43,6 +45,7 @@ class procedure TTestASTNode.TestAll;
 begin
   OK('InspectToken', TestInspectToken);
   OK('InspectBinaryOperationNode', TestInspectBinaryOperationNode);
+  OK('InspectNestedBinaryOperationNodes', TestInspectNestedBinaryOperationNodes);
 end;
 
 class function TTestASTNode.TestInspectBinaryOperationNode: Boolean;
@@ -56,13 +59,53 @@ begin
   AOp := MakeToken(TTTimesSign, '*');
   ARight := MakeToken(TTNumber, '9');
   ANode := TBinaryOperationNode.Create(ALeft, AOp, ARight);
-  Result := (ANode.Inspect = 'BinaryOperationNode' + #13#10 +
-    '  LeftNode: Number |6|' + #13#10 + '  OperatorNode: TimesSign |*|' +
-    #13#10 + '  RightNode: Number |9|');
+
+  Result := (ANode.Inspect =
+    'BinaryOperationNode' + #13#10 +
+    '  LeftNode: Number |6|' + #13#10 +
+    '  OperatorNode: TimesSign |*|' + #13#10 +
+    '  RightNode: Number |9|');
+
   ANode.Free;
   ALeft.Free;
   AOp.Free;
   ARight.Free;
+end;
+
+class function TTestASTNode.TestInspectNestedBinaryOperationNodes: Boolean;
+var
+  ALeftLeft: TASTNode;
+  ALeftOp: TToken;
+  ALeftRight: TASTNode;
+  ALeft: TASTNode;
+  AOp: TToken;
+  ARight: TASTNode;
+  ANode: TASTNode;
+begin
+  ALeftLeft := MakeToken(TTNumber, '2');
+  ALeftOp := MakeToken(TTTimesSign, '*');
+  ALeftRight := MakeToken(TTNumber, '3');
+  ALeft := TBinaryOperationNode.Create(ALeftLeft, ALeftOp, ALeftRight);
+  AOp := MakeToken(TTTimesSign, '*');
+  ARight := MakeToken(TTNumber, '9');
+  ANode := TBinaryOperationNode.Create(ALeft, AOp, ARight);
+
+  Result := (ANode.Inspect =
+    'BinaryOperationNode' + #13#10 +
+    '  LeftNode: BinaryOperationNode' + #13#10 +
+    '    LeftNode: Number |2|' + #13#10 +
+    '    OperatorNode: TimesSign |*|' + #13#10 +
+    '    RightNode: Number |3|' + #13#10 +
+    '  OperatorNode: TimesSign |*|' + #13#10 +
+    '  RightNode: Number |9|');
+
+  ANode.Free;
+  ALeft.Free;
+  AOp.Free;
+  ARight.Free;
+  ALeftLeft.Free;
+  ALeftOp.Free;
+  ALeftRight.Free;
 end;
 
 class function TTestASTNode.TestInspectToken: Boolean;
