24501f45bb4e624a734866790f92ec5e45635a5f
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-11 14:56:54 +0100

    UTestBareInherited: Added exception unit test

--- PasParseTest/UTestBareInherited.pas
+++ PasParseTest/UTestBareInherited.pas
@@ -15,7 +15,7 @@ type
 implementation
 
 uses
-  UTestParser, URuleType;
+  UTestParser, URuleType, UParseException;
 
 { TTestBareInherited }
 
@@ -28,6 +28,18 @@ class procedure TTestBareInherited.TestAll;
 begin
   OK('inherited', TTestParser.ParsesAs('inherited',
     'InheritedKeyword |inherited|', RTBareInherited));
+
+  try
+    TTestParser.ParsesAs('inherited Foo',
+      'InheritedKeyword |inherited|', RTBareInherited);
+    OK(False, 'inherited Foo exception');
+  except
+    on ETestException do;
+    on EParseException do
+      OK(True, 'inherited Foo exception');
+  else
+    OK(False, 'inherited Foo exception');
+  end;
 end;
 
 end.
