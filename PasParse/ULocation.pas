39dadf21a554d4352d586fab3919932b70e19200
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-11 10:11:31 +0100

    ULocation: Added Clone() function

--- PasParse/ULocation.pas
+++ PasParse/ULocation.pas
@@ -18,11 +18,18 @@ type
     property FileName: string read FFileName;
     property FileSource: string read FFileSource;
     property Offset: Integer read FOffset;
+
+    function Clone: TLocation;
   end;
 
 implementation
 
 { TLocation }
+
+function TLocation.Clone: TLocation;
+begin
+  Result := TLocation.Create(FFileName, FFileSource, FOffset);
+end;
 
 constructor TLocation.Create(AFileName, AFileSource: string; AOffset: Integer);
 begin
