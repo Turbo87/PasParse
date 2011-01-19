5a829b77739cbf189ed10222f72af1c96e36feba
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-19 09:50:42 +0100

    UTestParticle: Added remaining unit tests

--- PasParseTest/UTestParticle.pas
+++ PasParseTest/UTestParticle.pas
@@ -34,7 +34,26 @@ begin
   OK('Absolute', TTestParser.ParsesAs('Absolute',
     'Identifier |Absolute|', RTParticle));
 
-  // TODO
+  OK('(Foo)', TTestParser.ParsesAs('(Foo)',
+    'ParenthesizedExpressionNode' + #13#10 +
+    '  OpenParenthesisNode: OpenParenthesis |(|' + #13#10 +
+    '  ExpressionNode: Identifier |Foo|' + #13#10 +
+    '  CloseParenthesisNode: CloseParenthesis |)|', RTParticle));
+
+  OK('[1, 3..4]', TTestParser.ParsesAs('[1, 3..4]',
+    'SetLiteralNode' + #13#10 +
+    '  OpenBracketNode: OpenBracket |[|' + #13#10 +
+    '  ItemListNode: ListNode' + #13#10 +
+    '    Items[0]: DelimitedItemNode' + #13#10 +
+    '      ItemNode: Number |1|' + #13#10 +
+    '      DelimiterNode: Comma |,|' + #13#10 +
+    '    Items[1]: DelimitedItemNode' + #13#10 +
+    '      ItemNode: BinaryOperationNode' + #13#10 +
+    '        LeftNode: Number |3|' + #13#10 +
+    '        OperatorNode: DotDot |..|' + #13#10 +
+    '        RightNode: Number |4|' + #13#10 +
+    '      DelimiterNode: (none)' + #13#10 +
+    '  CloseBracketNode: CloseBracket |]|', RTParticle));
 
   OK('string', TTestParser.ParsesAs('string', 'StringKeyword |string|', RTParticle));
   OK('file', TTestParser.ParsesAs('file', 'FileKeyword |file|', RTParticle));
