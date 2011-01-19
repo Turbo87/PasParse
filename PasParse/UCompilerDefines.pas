7cf41d2ba3c58a84a465607207aa9bd8ba7225fc
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-19 09:47:22 +0100

    UCompilerDefines: Fixed compiler warning
    We shouldn't have two constructors with the same parameters (none) even though with different names...

--- PasParse/UCompilerDefines.pas
+++ PasParse/UCompilerDefines.pas
@@ -11,8 +11,7 @@ type
     FDictionary: TDictionary;
 
   public
-    constructor Create;
-    constructor CreateStandard;
+    constructor Create(ADefineStandards: Boolean = False);
     destructor Destroy; override;
 
     procedure DefineDirective(ACompilerDirective: string; AIsTrue: Boolean);
@@ -31,68 +30,66 @@ uses
 
 { TCompilerDefines }
 
-constructor TCompilerDefines.Create;
+constructor TCompilerDefines.Create(ADefineStandards: Boolean);
 begin
   FDictionary := TDictionary.Create;
-end;
 
-constructor TCompilerDefines.CreateStandard;
-begin
-  Create;
+  if ADefineStandards then
+  begin
+    DefineSymbol('CONDITIONALEXPRESSIONS');
+    DefineSymbol('CPU386');
+    DefineSymbol('MSWINDOWS');
+    DefineSymbol('WIN32');
+    DefineDirectiveAsTrue('IF not GenericSafeArrays');
+    DefineDirectiveAsTrue('IF not GenericVariants');
 
-  DefineSymbol('CONDITIONALEXPRESSIONS');
-  DefineSymbol('CPU386');
-  DefineSymbol('MSWINDOWS');
-  DefineSymbol('WIN32');
-  DefineDirectiveAsTrue('IF not GenericSafeArrays');
-  DefineDirectiveAsTrue('IF not GenericVariants');
+    // Delphi 1 for Win16
+    UndefineSymbol('WINDOWS');
+    // C++Builder
+    UndefineSymbol('BCB');
+    // Kylix
+    UndefineSymbol('DECLARE_GPL');
+    UndefineSymbol('ELF');
+    UndefineSymbol('LINUX');
+    UndefineSymbol('LINUX32');
+    UndefineSymbol('PC_MAPPED_EXCEPTIONS');
+    UndefineSymbol(' PC_MAPPED_EXCEPTIONS');
+    UndefineSymbol('PIC');
+    UndefineSymbol('POSIX');
+    UndefineSymbol('POSIX32');
+    // Delphi for .NET
+    UndefineSymbol('CIL');
+    UndefineSymbol('CLR');
+    UndefineSymbol('MANAGEDCODE');
+    // Miscellaneous
+    UndefineSymbol('DEBUG');
+    UndefineSymbol('DEBUG_FUNCTIONS');
+    UndefineSymbol('DEBUG_MREWS');
+    UndefineSymbol('GLOBALALLOC');
+    UndefineSymbol('MEMORY_DIAG');
+    UndefineSymbol('PUREPASCAL');
+    UndefineSymbol('TRIAL_EDITION');
+    UndefineSymbol('USEPASCALCODE');
+    DefineDirectiveAsFalse('IF Defined(PIC) Or Defined(PUREPASCAL)');
+    DefineDirectiveAsFalse('IF GenericOperations');
+    DefineDirectiveAsFalse('IF GenericVariants');
+    DefineDirectiveAsFalse('IFOPT R-');
 
-  // Delphi 1 for Win16
-  UndefineSymbol('WINDOWS');
-  // C++Builder
-  UndefineSymbol('BCB');
-  // Kylix
-  UndefineSymbol('DECLARE_GPL');
-  UndefineSymbol('ELF');
-  UndefineSymbol('LINUX');
-  UndefineSymbol('LINUX32');
-  UndefineSymbol('PC_MAPPED_EXCEPTIONS');
-  UndefineSymbol(' PC_MAPPED_EXCEPTIONS');
-  UndefineSymbol('PIC');
-  UndefineSymbol('POSIX');
-  UndefineSymbol('POSIX32');
-  // Delphi for .NET
-  UndefineSymbol('CIL');
-  UndefineSymbol('CLR');
-  UndefineSymbol('MANAGEDCODE');
-  // Miscellaneous
-  UndefineSymbol('DEBUG');
-  UndefineSymbol('DEBUG_FUNCTIONS');
-  UndefineSymbol('DEBUG_MREWS');
-  UndefineSymbol('GLOBALALLOC');
-  UndefineSymbol('MEMORY_DIAG');
-  UndefineSymbol('PUREPASCAL');
-  UndefineSymbol('TRIAL_EDITION');
-  UndefineSymbol('USEPASCALCODE');
-  DefineDirectiveAsFalse('IF Defined(PIC) Or Defined(PUREPASCAL)');
-  DefineDirectiveAsFalse('IF GenericOperations');
-  DefineDirectiveAsFalse('IF GenericVariants');
-  DefineDirectiveAsFalse('IFOPT R-');
-
-  // Version tags. Not surprisingly, you should DefineSymbol() only one.
-  UndefineSymbol('VER80');  // Delphi 1
-  UndefineSymbol('VER90');  // Delphi 2
-  UndefineSymbol('VER93');  // C++Builder 1
-  UndefineSymbol('VER100'); // Delphi 3
-  UndefineSymbol('VER110'); // C++Builder 3
-  UndefineSymbol('VER120'); // Delphi 4
-  UndefineSymbol('VER125'); // C++Builder 4
-  UndefineSymbol('VER130'); // Delphi/C++Builder 5
-  UndefineSymbol('VER140'); // Delphi/C++Builder 6, Kylix 1, 2, & 3
-  UndefineSymbol('VER150'); // Delphi 7
-  UndefineSymbol('VER160'); // Delphi 8 for .NET
-  UndefineSymbol('VER170'); // Delphi 2005
-  UndefineSymbol('VER180'); // Delphi 2006
+    // Version tags. Not surprisingly, you should DefineSymbol() only one.
+    UndefineSymbol('VER80');  // Delphi 1
+    UndefineSymbol('VER90');  // Delphi 2
+    UndefineSymbol('VER93');  // C++Builder 1
+    UndefineSymbol('VER100'); // Delphi 3
+    UndefineSymbol('VER110'); // C++Builder 3
+    UndefineSymbol('VER120'); // Delphi 4
+    UndefineSymbol('VER125'); // C++Builder 4
+    UndefineSymbol('VER130'); // Delphi/C++Builder 5
+    UndefineSymbol('VER140'); // Delphi/C++Builder 6, Kylix 1, 2, & 3
+    UndefineSymbol('VER150'); // Delphi 7
+    UndefineSymbol('VER160'); // Delphi 8 for .NET
+    UndefineSymbol('VER170'); // Delphi 2005
+    UndefineSymbol('VER180'); // Delphi 2006
+  end;
 end;
 
 procedure TCompilerDefines.DefineDirective(ACompilerDirective: string;
