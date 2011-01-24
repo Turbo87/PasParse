06e67851a5f93027a2ffcb13d3b2110c0919db2d
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-24 14:12:11 +0100

    UITokenSet: Added Documentation

--- PasParse/UITokenSet.pas
+++ PasParse/UITokenSet.pas
@@ -6,14 +6,21 @@ uses
   UTokenType;
 
 type
+  /// <Description>Prototype/Interface for a TokenSet</Description>
   ITokenSet = class
   protected
+    /// <Description>Returns the name of the TokenSet</Description>
+    /// <Description>Must be implemented by all derived classes.</Description>
+    /// <Description>Use the public Name property to access!</Description>
     function GetName: string; virtual; abstract;
 
   public
-    /// Checks whether the set contains the given token type
+    /// <Description>Checks whether the set contains the given TokenType.</Description>
+    /// <Description>Must be implemented by all derived classes.</Description>
     function Contains(ATokenType: TTokenType): Boolean; virtual; abstract;
 
+    /// <Description>Returns the name of the TokenSet.</Description>
+    /// <Description>Calls the GetName() function.</Description>
     property Name: string read GetName;
   end;
 
