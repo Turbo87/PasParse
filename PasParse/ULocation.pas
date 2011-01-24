31e52d4a22da0d520efe94367e3000e3e90f65f1
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-24 18:18:10 +0100

    ULocation: Added Documentation

--- PasParse/ULocation.pas
+++ PasParse/ULocation.pas
@@ -3,19 +3,28 @@ unit ULocation;
 interface
 
 type
+  /// <Description>A class holding information about a certain Location in a code file.</Description>
   TLocation = class
   private
+    /// <Description>The name of the file where the Location can be found.</Description>
     FFileName: string;
+    /// <Description>The content of the file where the Location can be found.</Description>
     FFileSource: string;
+    /// <Description>The offset from the start of FileSource where the Location can be found.</Description>
     FOffset: Integer;
 
   public
+    /// <Description>Default constructor.</Description>
     constructor Create(AFileName, AFileSource: string; AOffset: Integer);
 
+    /// <Description>The name of the file where the Location can be found.</Description>
     property FileName: string read FFileName;
+    /// <Description>The content of the file where the Location can be found.</Description>
     property FileSource: string read FFileSource;
+    /// <Description>The offset from the start of FileSource where the Location can be found.</Description>
     property Offset: Integer read FOffset;
 
+    /// <Description>Creates a clone of the current Location</Description>
     function Clone: TLocation;
   end;
 
@@ -25,11 +34,13 @@ implementation
 
 function TLocation.Clone: TLocation;
 begin
+  // Create a new Location instance with the same parameters and return it
   Result := TLocation.Create(FFileName, FFileSource, FOffset);
 end;
 
 constructor TLocation.Create(AFileName, AFileSource: string; AOffset: Integer);
 begin
+  // Assign private fields
   FFileName := AFileName;
   FFileSource := AFileSource;
   FOffset := AOffset;
