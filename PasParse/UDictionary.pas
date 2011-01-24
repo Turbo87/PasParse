0b656a20e88d521f2d428150ec430f1378eabe5c
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-01-24 18:02:09 +0100

    UDictionary: Added Documentation

--- PasParse/UDictionary.pas
+++ PasParse/UDictionary.pas
@@ -6,37 +6,40 @@ uses
   Classes;
 
 type
+  /// <Description>A generic Dictionary class.</Description>
+  /// <Description>Instead of an Integer-based array index a string is used.</Description>
   TDictionary = class
   private
-    /// Internal list of the dictionary
+    /// <Description>Internal list of the Dictionary.</Description>
     FList: TStringList;
-    /// Should the destructor free its children in the destructor
+    /// <Description>Should the destructor free its children in the destructor?</Description>
     FFreeChildren: Boolean;
 
-    /// Returns the integer-based index of the given key or -1 if not found
+    /// <Description>Returns the integer-based index of the given key or -1 if not found.</Description>
     function IndexOf(const AKey: string): Integer;
 
+    /// <Description>Redirection to Write() without return value.</Description>
+    /// <Description>Used to enable write support for the Items property.</Description>
     procedure WriteRedirection(const AKey: string; const AValue: TObject);
 
   public
-    /// Standard constructor
+    /// <Description>Default constructor.</Description>
     constructor Create(const AFreeChildren: Boolean = False);
-    /// Standard destructor
+    /// <Description>Default destructor.</Description>
     destructor Destroy; override;
 
-    /// Returns true if the given key was found in the dictionary and writes
-    ///  the corresponding TObject instance to the AValue output parameter
+    /// <Description>Returns true if the given key was found in the Dictionary.</Description>
+    /// <Description>Writes the corresponding TObject instance to the Value output parameter.</Description>
     function Read(const AKey: string; out AValue: TObject): Boolean; overload;
-    /// Returns the TObject instance corresponding with the given key or
-    ///  nil if not found
+    /// <Description>Returns the TObject instance corresponding with the Key or nil if not found.</Description>
     function Read(const AKey: string): TObject; overload;
-    /// Assigns the given TObject to the given key.
-    ///  Returns whether the key already existed before.
+    /// <Description>Assigns Value to Key. Overwrites if Key exists already.</Description>
     function Write(const AKey: string; const AValue: TObject): Boolean;
 
-    /// Tests whether a key exists in the dictionary
+    /// <Description>Checks whether the Key exists in the Dictionary.</Description>
     function Contains(const AKey: string): Boolean;
 
+    /// <Description>User-friendly syntax option to access the Read/Write() functions.</Description>
     property Items[const AKey: string]: TObject read Read write WriteRedirection;
   end;
 
@@ -48,7 +51,9 @@ function TDictionary.Contains(const AKey: string): Boolean;
 var
   AIndex: Integer;
 begin
+  // Try to find the Key in the interal StringList
   AIndex := IndexOf(AKey);
+  // IndexOf returns negative value if Key was not found!
   Result := (AIndex >= 0);
 end;
 
@@ -56,6 +61,7 @@ constructor TDictionary.Create(const AFreeChildren: Boolean);
 begin
   inherited Create;
   FFreeChildren := AFreeChildren;
+  // Initialize internal list
   FList := TStringList.Create;
 end;
 
@@ -63,20 +69,25 @@ destructor TDictionary.Destroy;
 var
   I: Integer;
 begin
+  // If the dictionary is supposed to kill it's children
   if FFreeChildren then
   begin
+    // ... iterate through the children
     for I := 0 to FList.Count - 1 do
     begin
+      // ... and free them 
       FList.Objects[I].Free;
     end;
   end;
 
+  // Destroy the internal list
   FList.Free;
   inherited;
 end;
 
 function TDictionary.IndexOf(const AKey: string): Integer;
 begin
+  // Pass the result of the internal IndexOf() function
   Result := FList.IndexOf(AKey);
 end;
 
@@ -84,9 +95,11 @@ function TDictionary.Read(const AKey: string; out AValue: TObject): Boolean;
 var
   AIndex: Integer;
 begin
+  // Try to find the Key
   AIndex := IndexOf(AKey);
   Result := (AIndex >= 0);
   if Result then
+    // ... and if we've found it return the Value as output parameter
     AValue := FList.Objects[AIndex]
 end;
 
@@ -94,9 +107,12 @@ function TDictionary.Read(const AKey: string): TObject;
 var
   AValue: TObject;
 begin
+  // Try to read the Key
   if Read(AKey, AValue) = True then
+    // ... and if successful return the Value
     Result := AValue
   else
+    // ... otherwise return nil
     Result := nil;
 end;
 
@@ -104,22 +120,26 @@ function TDictionary.Write(const AKey: string; const AValue: TObject): Boolean;
 var
   AIndex: Integer;
 begin
+  // Try to find the Key
   AIndex := IndexOf(AKey);
   Result := (AIndex >= 0);
   if Result then
   begin
+    // ... and if we've found it overwrite (and free) the old value
     if FFreeChildren then
         FList.Objects[AIndex].Free;
 
     FList.Objects[AIndex] := AValue
   end
   else
+    // ... otherwise add a new key-value pair
     FList.AddObject(AKey, AValue);
 end;
 
 procedure TDictionary.WriteRedirection(const AKey: string;
   const AValue: TObject);
 begin
+  // Pass Key and Value to Write() and don't care about the return value
   Write(AKey, AValue);
 end;
 
