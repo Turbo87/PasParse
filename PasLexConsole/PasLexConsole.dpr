18f55b4fa2e458318e1534e0e90c8a70b4ddb032
Tobias Bieniek <tobias.bieniek@arc-aachen.de>
2011-02-02 11:13:41 +0100

    PasLexConsole: Added Documentation

--- PasLexConsole/PasLexConsole.dpr
+++ PasLexConsole/PasLexConsole.dpr
@@ -12,16 +12,22 @@ var
   ALexScanner: TLexScanner;
   AToken: TToken;
 begin
+  // Create FileLoader to load the specified file
   AFileLoader := TFileLoader.Create;
   try
+    // Load the file content
     AContent := AFileLoader.Load(AFileName);
+    // Create the lexer
     ALexScanner := TLexScanner.Create(AContent, AFileName);
     try
+      // Read tokens
       repeat
         AToken := ALexScanner.NextToken;
+        // Last token reached?
         if AToken = nil then
           Break;
 
+        // Write token type and token text to stdout
         WriteLn(
           GetEnumName(TypeInfo(TTokenType), Integer(AToken.TokenType)) + ': ''' +
           AToken.Text + '''');
@@ -39,9 +45,11 @@ end;
 
 begin
   try
+    // No file given as parameter
     if ParamCount < 1 then
       raise Exception.Create('missing file parameter');
 
+    // Lex given filename
     LoadFile(ParamStr(1));
   except
     on E: Exception do
